50f32904e2cd35d70b1c804581563a07450391a3
Manish Jain <jainmanish01995@gmail.com>
2016-11-09 23:12:38 +0530

    some change

--- auth.php
+++ auth.php
@@ -1,6 +1,8 @@
 <?php
-session_start();
-if(!isset($_SESSION["username"])){
-header("Location: login.php");
-exit(); }
+	session_start();
+	
+	if(!isset($_SESSION["username"])){
+		header("Location: login.php");
+		exit(); 
+	}
 ?>
