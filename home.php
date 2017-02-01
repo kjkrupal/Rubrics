5a14c6303a3865e5f16df26d519cb7099b005d67
SushantYadav <yadavsushant321@gmail.com>
2017-02-01 15:34:09 +0530

    Homepage UI Added

--- home.php
+++ home.php
@@ -5,11 +5,25 @@ include_once 'dbConfig.php';
 <!DOCTYPE html>
 <html>
 <head>
+<style>
+    .button{
+        background-color: #4CAF50;
+        border: none;
+        color: whitesmoke;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+    }
+</style>
     <title>Home</title>
     
 </head>
 <body>
-
+<form>
     <?php if (isset($_SESSION['teacher_id'])) { ?>
     Signed in as <?php echo $_SESSION['teacher_name']; ?><br><br>
     
@@ -21,10 +35,18 @@ include_once 'dbConfig.php';
     <a href="javascript://" onclick="self.parent.location='logout.php'" target="abc">Logout</a>
 
     <?php } else { ?>
-
-    You are not Logged in! Kindly <a href="login.php">login here</a><br>
-    New user? <a href="register.php">Sign Up here</a>
+<br>
+<br>
+<center>
+<a href="Login.php" class="button">Click to login</a>
+<a href="Register.php" class="button">Click to Register</a>
+</center>
+<!-- <center><input type="button" value="Click to login" onclick="window.location.href='Login.php'"/>
+    <input type="button" value="Click to Register" onclick="window.location.href='Register.php'"/></center> -->
+    
+<!--   You are not Logged in! Kindly <a href="login.php">login here</a><br>
+    New user? <a href="register.php">Sign Up here</a> -->
     <?php } ?>
-
+</form>
 </body>
 </html>
