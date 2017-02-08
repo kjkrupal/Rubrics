36e1d0c91c67705a21b513b3aac07be663c7620d
DeepeshGupta <deepesh.gupta96@gmail.com>
2017-02-08 10:47:59 +0530

    Manage Course created

---
+++ managecourse.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Course</title>
+</head>
+<body>
+
+<?php
+session_start();
+include 'dbConfig.php';
+
+if(isset($_GET['action'] )){
+	if(!strcmp($_GET['action'], "deleteCourse")){
+		$cname=$_GET['cn'];
+		$db->query("DELETE FROM course WHERE coid=".$cname. ";");
+		
+	}
+}
+
+	$classname=$db->query("SELECT classname,cid FROM class WHERE tid=".$_SESSION['teacher_id']);   
+   while($row = $classname->fetch_assoc())
+   {
+   	?> <h2><?php echo $row['classname'];?></h2>
+   <br>
+		<table>
+      		<thead>
+            <tr>
+                <th><h4>Course</h4></th>
+                <th><h4>Action</h4></th>
+            </tr>
+      		</thead>
+      </table>
+
+   <?php
+	$course=$db->query("SELECT coursename, coid FROM course WHERE cid=".$row['cid']);
+	while($row = $course->fetch_assoc())
+	{
+	 	?><br>
+	 	<table>
+	 	<tr>
+	 	<td><?php echo $row['coursename']; ?></td>
+	 	<td><a href="managecourse.php?action=deleteCourse&cn=<?php echo $row['coid'];?>">Delete</a></td>
+	 	</tr>
+	 	</table>
+	<?php  } ?>
+	 <br>
+	 <?php 
+	 } ?>
+</body>
+</html>
