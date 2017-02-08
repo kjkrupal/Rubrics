e94aeef2330fe7133df5553691f0de83ca8128dd
SushantYadav <yadavsushant321@gmail.com>
2017-02-08 11:21:30 +0530

    Added External Stylesheet

--- Login.php
+++ Login.php
@@ -24,73 +24,20 @@ if (isset($_POST['login'])) {
 ?>
 <!DOCTYPE html>
 <html>
-<style>
-form {
-    border: 3px solid #f1f1f1;
-}
-
-input[type=text], input[type=password] {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    box-sizing: border-box;
-}
-
-button {
-    background-color: #4CAF50;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    cursor: pointer;
-    width: 100%;
-}
-
-.imgcontainer {
-    text-align: center;
-    margin: 22px 0 12px 0;
-}
-
-img.avatar {
-    width: 40%;
-    border-radius: 50%;
-}
-
-.container {
-    padding: 16px;
-}
-
-span.password {
-    float: right;
-    padding-top: 16px;
-}
-
-/* Change styles for span and cancel button on extra small screens */
-@media screen and (max-width: 300px) {
-    span.password {
-       display: block;
-       float: none;
-    }
-}
-</style>
 <body>
 
 <h2>Login</h2>
 
 <form role="form" action="Login.php" method="post" name="loginform">
-    <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar">
     </div>
 
     <div class="container">
         <label><b>email</b></label>
-        <input type="text" placeholder="email" name="email" required>
+        <input type="text" placeholder="email" name="email" required><br>
 
         <label><b>password</b></label>
-        <input type="password" placeholder="password" name="password" required>
-        <center><input type="submit" name="login" value="Login"/></center>
+        <input type="password" placeholder="password" name="password" required><br>
+        <input type="submit" name="login" value="Login"/>
     </div>
     New User? <a href="register.php">Sign Up Here</a>
 </form>
