c4d097aa84ebab5786c9317d44c57d7466a856a7
Deepesh Gupta <deepesh.gupta96@gmail.com>
2017-03-19 09:54:36 +0530

    modified database

--- rubric.sql
+++ rubric.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 12, 2017 at 01:16 PM
+-- Generation Time: Mar 19, 2017 at 04:23 AM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -19,6 +19,68 @@ SET time_zone = "+00:00";
 --
 -- Database: `rubric`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `2_grade`
+--
+
+CREATE TABLE `2_grade` (
+  `tablegrade_id` int(11) NOT NULL,
+  `coursename` varchar(100) DEFAULT NULL,
+  `coid` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `2_grade`
+--
+
+INSERT INTO `2_grade` (`tablegrade_id`, `coursename`, `coid`) VALUES
+(1, 'SE_grade', 12),
+(2, 'SE_grade', 12);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assignment2level2`
+--
+
+CREATE TABLE `assignment2level2` (
+  `level_id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `minGrade` int(11) DEFAULT NULL,
+  `maxGrade` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `assignment2level2`
+--
+
+INSERT INTO `assignment2level2` (`level_id`, `name`, `minGrade`, `maxGrade`) VALUES
+(1, 'excellent', 8, 10),
+(2, 'Good', 7, 5),
+(3, 'Poor', 0, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assignment2parameter2`
+--
+
+CREATE TABLE `assignment2parameter2` (
+  `param_id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `assignment2parameter2`
+--
+
+INSERT INTO `assignment2parameter2` (`param_id`, `name`) VALUES
+(1, 'Aim'),
+(2, 'Theory'),
+(3, 'Result');
 
 -- --------------------------------------------------------
 
@@ -84,15 +146,16 @@ CREATE TABLE `course` (
 --
 
 INSERT INTO `course` (`coid`, `coursename`, `cid`, `tid`, `coursetable`) VALUES
+(12, 'SE', 8, 2, 'SE2'),
 (11, 'SNMR', 6, 2, 'SNMR2');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `projectlevel2`
+-- Table structure for table `practicallevel2`
 --
 
-CREATE TABLE `projectlevel2` (
+CREATE TABLE `practicallevel2` (
   `level_id` int(11) NOT NULL,
   `name` varchar(100) DEFAULT NULL,
   `minGrade` int(11) DEFAULT NULL,
@@ -100,34 +163,35 @@ CREATE TABLE `projectlevel2` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `projectlevel2`
+-- Dumping data for table `practicallevel2`
 --
 
-INSERT INTO `projectlevel2` (`level_id`, `name`, `minGrade`, `maxGrade`) VALUES
-(1, 'Excellent', 0, 0),
-(2, 'Good', 0, 0),
-(3, 'Average', 0, 0),
-(4, 'Poor', 0, 0);
+INSERT INTO `practicallevel2` (`level_id`, `name`, `minGrade`, `maxGrade`) VALUES
+(1, 'excellent', 8, 10),
+(2, 'Good', 7, 5),
+(3, 'Bad', 2, 4),
+(4, 'Poor', 0, 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `projectparameter2`
+-- Table structure for table `practicalparameter2`
 --
 
-CREATE TABLE `projectparameter2` (
+CREATE TABLE `practicalparameter2` (
   `param_id` int(11) NOT NULL,
   `name` varchar(100) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `projectparameter2`
+-- Dumping data for table `practicalparameter2`
 --
 
-INSERT INTO `projectparameter2` (`param_id`, `name`) VALUES
-(1, 'Handwriting'),
-(2, 'Content'),
-(3, 'Theory');
+INSERT INTO `practicalparameter2` (`param_id`, `name`) VALUES
+(1, 'Aim'),
+(2, 'Theory'),
+(3, 'Procedure'),
+(4, 'Result');
 
 -- --------------------------------------------------------
 
@@ -163,7 +227,40 @@ CREATE TABLE `rubrics` (
 --
 
 INSERT INTO `rubrics` (`rid`, `rubricname`, `tid`) VALUES
-(18, 'project', 2);
+(27, 'assignment2', 2),
+(26, 'practical', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `se2`
+--
+
+CREATE TABLE `se2` (
+  `assignment_id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `Deadline` varchar(100) DEFAULT NULL,
+  `grade` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `se2`
+--
+
+INSERT INTO `se2` (`assignment_id`, `name`, `Deadline`, `grade`) VALUES
+(1, 'practical 1', '2017-03-12', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `se_grade`
+--
+
+CREATE TABLE `se_grade` (
+  `coursegrade_id` int(11) NOT NULL,
+  `assignment_name` varchar(100) DEFAULT NULL,
+  `assignment_id` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -184,6 +281,26 @@ CREATE TABLE `snmr2` (
 
 INSERT INTO `snmr2` (`assignment_id`, `name`, `Deadline`, `grade`) VALUES
 (1, 'CBS', '2017-03-15', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `startgrading`
+--
+
+CREATE TABLE `startgrading` (
+  `grading_id` int(100) NOT NULL,
+  `tid_grade` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `startgrading`
+--
+
+INSERT INTO `startgrading` (`grading_id`, `tid_grade`) VALUES
+(40, '2_grade'),
+(39, '2_grade'),
+(38, '2_grade');
 
 -- --------------------------------------------------------
 
@@ -246,6 +363,24 @@ INSERT INTO `users` (`teacher_id`, `name`, `email`, `password`) VALUES
 --
 
 --
+-- Indexes for table `2_grade`
+--
+ALTER TABLE `2_grade`
+  ADD PRIMARY KEY (`tablegrade_id`);
+
+--
+-- Indexes for table `assignment2level2`
+--
+ALTER TABLE `assignment2level2`
+  ADD PRIMARY KEY (`level_id`);
+
+--
+-- Indexes for table `assignment2parameter2`
+--
+ALTER TABLE `assignment2parameter2`
+  ADD PRIMARY KEY (`param_id`);
+
+--
 -- Indexes for table `be_it2`
 --
 ALTER TABLE `be_it2`
@@ -265,15 +400,15 @@ ALTER TABLE `course`
   ADD PRIMARY KEY (`coid`);
 
 --
--- Indexes for table `projectlevel2`
+-- Indexes for table `practicallevel2`
 --
-ALTER TABLE `projectlevel2`
+ALTER TABLE `practicallevel2`
   ADD PRIMARY KEY (`level_id`);
 
 --
--- Indexes for table `projectparameter2`
+-- Indexes for table `practicalparameter2`
 --
-ALTER TABLE `projectparameter2`
+ALTER TABLE `practicalparameter2`
   ADD PRIMARY KEY (`param_id`);
 
 --
@@ -289,10 +424,28 @@ ALTER TABLE `rubrics`
   ADD PRIMARY KEY (`rid`);
 
 --
+-- Indexes for table `se2`
+--
+ALTER TABLE `se2`
+  ADD PRIMARY KEY (`assignment_id`);
+
+--
+-- Indexes for table `se_grade`
+--
+ALTER TABLE `se_grade`
+  ADD PRIMARY KEY (`coursegrade_id`);
+
+--
 -- Indexes for table `snmr2`
 --
 ALTER TABLE `snmr2`
   ADD PRIMARY KEY (`assignment_id`);
+
+--
+-- Indexes for table `startgrading`
+--
+ALTER TABLE `startgrading`
+  ADD PRIMARY KEY (`grading_id`);
 
 --
 -- Indexes for table `temp_data`
@@ -320,6 +473,21 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `2_grade`
+--
+ALTER TABLE `2_grade`
+  MODIFY `tablegrade_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `assignment2level2`
+--
+ALTER TABLE `assignment2level2`
+  MODIFY `level_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `assignment2parameter2`
+--
+ALTER TABLE `assignment2parameter2`
+  MODIFY `param_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT for table `be_it2`
 --
 ALTER TABLE `be_it2`
@@ -333,27 +501,42 @@ ALTER TABLE `class`
 -- AUTO_INCREMENT for table `course`
 --
 ALTER TABLE `course`
-  MODIFY `coid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `coid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
--- AUTO_INCREMENT for table `projectlevel2`
+-- AUTO_INCREMENT for table `practicallevel2`
 --
-ALTER TABLE `projectlevel2`
+ALTER TABLE `practicallevel2`
   MODIFY `level_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
--- AUTO_INCREMENT for table `projectparameter2`
+-- AUTO_INCREMENT for table `practicalparameter2`
 --
-ALTER TABLE `projectparameter2`
-  MODIFY `param_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `practicalparameter2`
+  MODIFY `param_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `rubrics`
 --
 ALTER TABLE `rubrics`
-  MODIFY `rid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `rid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+--
+-- AUTO_INCREMENT for table `se2`
+--
+ALTER TABLE `se2`
+  MODIFY `assignment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `se_grade`
+--
+ALTER TABLE `se_grade`
+  MODIFY `coursegrade_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `snmr2`
 --
 ALTER TABLE `snmr2`
   MODIFY `assignment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `startgrading`
+--
+ALTER TABLE `startgrading`
+  MODIFY `grading_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 --
 -- AUTO_INCREMENT for table `temp_data`
 --
